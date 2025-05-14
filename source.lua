4ffe84396671eca44fab0f0bf6bfa4eae505b6a3
DELETIONSs <officialkadencreations@gmail.com>
2025-05-14 19:45:05 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -19,14 +19,6 @@ local LinkerLib = {
 			Divider = Color3.fromRGB(60, 60, 60),
 			Text = Color3.fromRGB(240, 240, 240),
 			TextDark = Color3.fromRGB(150, 150, 150)
-		},
-		Dark1 {
-			Main = Color3.fromRGB(25, 25, 25),
-			Second = Color3.fromRGB(32, 32, 32),
-			Stroke = Color3.fromRGB(60, 60, 60),
-			Divider = Color3.fromRGB(60, 60, 60),
-			Text = Color3.fromRGB(255, 255, 255),
-			TextDark = Color3.fromRGB(150, 150, 150)
 		}
 	},
 	SelectedTheme = "Default",
